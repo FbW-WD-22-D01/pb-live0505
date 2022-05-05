872d482a8403e19964c637da90f129af30831bfc
puckfried <puckfried@posteo.de>
2022-05-05 09:54:07 +0200

    deklaration

---
+++ index.js
@@ -0,0 +1,19 @@
+// ARRAYS
+
+// GEORDNETE LISTE
+
+// Variante 1 - Array deklarieren
+let meinArray = ["Ball", "Murmel", "Gitarre"] ;
+console.log(meinArray);
+
+
+// Variante 2 mit Kontruktor 
+let array2 = new Array("Ball", "Murmel", "Kugel");
+let arrLeer = new Array(5);
+
+
+// Die Länge eine Arrays ausgeben
+let string = "Ich bin ein String";
+console.log('Länge des Strings: ',string.length);
+console.log(array2.length);
+
